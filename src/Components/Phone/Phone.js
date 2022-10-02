6938a5ef23be65e73e14e696712be58f7b834c0f
sajid9962 <clashking1545@gmail.com>
2022-10-02 21:56:33 +0600

    input field made dynamic

--- src/Components/Phone/Phone.js
+++ src/Components/Phone/Phone.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { DevicePhoneMobileIcon } from '@heroicons/react/24/solid';
 
-const Phone = ({phone}) => {
+const Phone = ({ phone }) => {
+	const { phone_name, image, brand, slug } = phone;
+	console.log(phone);
 
-    const { phone_name, image, brand, slug } = phone;
-    console.log(phone);
-
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<div>
+			<div className="card w-96 bg-base-100 shadow-xl hover:shadow-red-700 duration-500 ease-linear hover:scale-95">
+				<figure className="px-10 pt-10">
+					<img src={image} alt="phones" className="rounded-xl" />
+				</figure>
+				<div className="card-body items-center text-center">
+					<h3 className="text-2xl font-bold"> Brand : {brand}</h3>
+					<h2 className="card-title">
+						
+						<DevicePhoneMobileIcon className="h-6 w-6" />  {phone_name}
+					</h2>
+					<div className="card-actions mt-3 w-full">
+						<button className="btn btn-primary w-full hover:bg-orange-700 duration-500 focus:bg-red-700 ">Buy Now</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Phone;
--- src/Components/Phones/Phones.js
+++ src/Components/Phones/Phones.js
@@ -2,29 +2,30 @@ import React, { useEffect, useState } from 'react';
 import Phone from '../Phone/Phone';
 
 const Phones = () => {
-	const [phones, setPhones] = useState([]);
+    const [phones, setPhones] = useState([]);
+	const [search, setSearch] = useState('');
+    
 
 	useEffect(() => {
-		fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`)
+		fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`)
 			.then((res) => res.json())
 			.then((data) => setPhones(data.data));
-	
-        }, []);
-
-    const [search, setSearch] = useState([]);
+	}, [search]);
 
 
 	return (
 		<div>
-			<input
+			<input onChange={(e)=> setSearch(e.target.value)}
 				type="text"
 				placeholder="Type here"
 				className="input input-bordered input-error w-full max-w-xs"
-            />
-            
-            {
-                phones.map(phone => <Phone key={phone.slug} phone={ phone } ></Phone>)
-            }
+			/>
+
+			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-[1300px] mx-auto gap-5 mt-5'>
+				{phones.map((phone) => (
+					<Phone key={phone.slug} phone={phone}></Phone>
+				))}
+			</div>
 		</div>
 	);
 };
