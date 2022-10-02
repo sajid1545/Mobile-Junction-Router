831bfcfb61a72318b8c9a79cba4f52180fd12bb3
sajid9962 <clashking1545@gmail.com>
2022-10-02 21:20:16 +0600

    navbar made responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
+	const [open, setOpen] = useState(false);
+
 	return (
-		<div>
-			
-            <ul className="text-2xl md:flex justify-center mt-2 gap-8 mb-10">
-            <img className='w-[40px] height-[100px] rounded-full'
-				src="https://img.freepik.com/free-vector/digital-device-mockup_53876-89354.jpg?w=740&t=st=1664722641~exp=1664723241~hmac=feb3e31e89629c65d73ae26e5bb1bf6bb75e49827792a05b4b5aede0c0208798" 
-				alt=""
-			/>
+		<nav className='w-full relative'>
+			<div onClick={() => setOpen(!open)} className="h-6 w-6 text-black-500 md:hidden  ">
+				{open ? <XMarkIcon></XMarkIcon> : <Bars3Icon></Bars3Icon>}
+			</div>
+
+			<ul
+				className={`text-2xl md:flex justify-center mt-2 gap-8 mb-10 md:static relative w-full ${
+					open ? 'top-6' : 'top-[-250px]'
+				}`}>
+				<img
+					className="w-[40px] height-[100px] rounded-full mx-auto md:m-0"
+					src="https://img.freepik.com/free-vector/digital-device-mockup_53876-89354.jpg?w=740&t=st=1664722641~exp=1664723241~hmac=feb3e31e89629c65d73ae26e5bb1bf6bb75e49827792a05b4b5aede0c0208798"
+					alt=""
+				/>
 				<li>
 					<Link to={`/home`}>Home</Link>
 				</li>
@@ -20,7 +30,7 @@ const Navbar = () => {
 					<Link to={`/about`}>About</Link>
 				</li>
 			</ul>
-		</div>
+		</nav>
 	);
 };
 
